bf6642b4ddcb6242dcccc74cecbcee3f67c05c11
atho219 <atho219@lsu.edu>
2016-11-11 18:20:18 -0600

    finished set eval

--- prog2/Csharp/Special/Set.cs
+++ prog2/Csharp/Special/Set.cs
@@ -12,6 +12,27 @@ namespace Tree
         {
             Printer.printSet(t, n, p);
         }
-    }
+
+        public override Node eval(Node exp, Environment env)
+        {
+            int numArgs = 0;
+            Node expCdr = exp.getCdr();
+            while (!expCdr.isNull())
+            {
+                numArgs++;
+                expCdr = expCdr.getCdr();
+            }
+
+            if (numArgs != 2)
+            {
+                Console.Error.WriteLine("Error: wrong number of arguments");
+                return Nil.getInstance();
+            }
+            Node arg1 = exp.getCdr().getCar();
+            Node arg2 = exp.getCdr().getCdr().getCar();
+            env.assign(arg1, arg2.eval(env));
+            return new StringLit("unspecified value");
+        }
+}
 }
 
